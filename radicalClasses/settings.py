53111fcaa2df98ddc33933d0d5653037f7a654eb
amanmehra <amanmehra262001@gmail.com>
2023-01-14 15:39:49 +0530

    django POC complete

--- radicalClasses/settings.py
+++ radicalClasses/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*x%bz#tq@umc(4-h9sksdgiq2jb@uh#_g-isc09mx2(x(3dv1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
